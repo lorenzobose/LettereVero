b67aa0a74900c38337d422bdf5526d4fb33a6d57
Lorenzo Boselli <74327107+lorenzobose@users.noreply.github.com>
2025-10-01 00:58:39 +0200

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,31 @@
+const body = document.createElement("div"); 
+body.classList.add("body");
+
+const leftFlap = document.createElement("div");
+leftFlap.classList.add("left-flap")
+
+const rightFlap = document.createElement("div");
+rightFlap.classList.add("right-flap")
+
+const topFlap = document.createElement("div");
+topFlap.classList.add("top-flap")
+
+field.appendChild(body);
+field.appendChild(leftFlap);
+field.appendChild(rightFlap);
+field.appendChild(topFlap);
+
+const card = document.createElement("div");
+card.classList.add("card");
+field.appendChild(card)
+card.innerHTML = "CIAO AMORE TI AMO TANTOOO"
+
+setTimeout(()=>{
+    topFlap.style.transform = "rotateX(180deg)";
+    topFlap.style.zIndex = "-4";
+    setTimeout(() => {
+        card.style.height = "400px";
+        card.style.top = "35%";
+    }, 1000);
+}, 3000)
+
